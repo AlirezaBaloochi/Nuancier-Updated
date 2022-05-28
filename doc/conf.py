9849ca69ca8937ec666680bf02e5f3015aefc475
fillthegap <alirezab806@gmail.com>
2022-05-28 16:41:24 +0400

    fixed navbar

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'nuancier'
-copyright = u'2013-2014, Red Hat Inc. - Pierre-Yves Chibon <pingou@pingoured.fr>'
+copyright = u'2013-2022, Red Hat Inc. - Pierre-Yves Chibon <pingou@pingoured.fr>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
